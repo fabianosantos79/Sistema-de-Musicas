ef6067d88d3d85759b021edd8bc3322f98872623
Fabiano <fabiano.ssantos79@gmail.com>
2022-04-11 23:43:25 -0300

    Commit do Sistema

---
+++ app.js
@@ -0,0 +1,10 @@
+const apiUrl = `https://api.lyrics.ovh/`;
+
+const getForm = document.querySelector('#form');
+const getInput = document.querySelector('#search');
+const getButton = document.querySelector('button');
+const getUlSongs = document.querySelector('#songs-container');
+const getPrevNext = document.querySelector('#prev-and-next-container');
+
+//console.log({getForm, getInput, getButton, getUlSongs, getPrevNext});
+
